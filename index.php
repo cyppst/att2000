152030d0134680d3ffbf3615e6d97c98cd6ffcb7
Golfcomsci57 <golfcomsci57@gmail.com>
2018-05-12 19:01:57 +0700

    select

--- index.php
+++ index.php
@@ -4,16 +4,12 @@ require_once('config/database.php');
 $records = $sql_conn->table('CHECKINOUT')->findAll();
 
 foreach ($records as $record) {
-    $mysql_conn->table('checkinout')->save([
+    $mysql_conn->table('times')->save([
         'USERID' => $record['USERID'],
         'CHECKTIME' => $record['CHECKTIME'],
         'CHECKTYPE' => $record['CHECKTYPE']
     ]);
 }
-$count = $mysql_conn->table('checkinout')->count();
-
-session_start();
-$_SESSION['message'] = 'รายการทั้งหมด '.$count.' แถว';
 
 header("location:javascript://history.go(-1)");
 exit;
---
+++ select.php
@@ -0,0 +1,13 @@
+<?php
+require_once('config/database.php');
+$records = $sql_conn->table('USERINFO')->findAll();
+?>
+<div class="form-group">
+    <label for="" class="col-sm-2 control-label"></label>
+
+    <select class="form-control" id="">
+        <? foreach ($records as $record) { ?>
+            <option value="<?= $record['USERID'] ?>"><?= $record['NAME'].'( '.$record['USERID'].' )' ?></option>
+        <? } ?>
+    </select>
+</div>
